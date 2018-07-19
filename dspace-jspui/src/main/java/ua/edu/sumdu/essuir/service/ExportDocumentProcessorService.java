b2d542c4f927db47ce83506aec8cc83dbbee62cf
Igor Marchenko <mar4enko.igor@gmail.com>
2018-07-19 15:00:10 +0300

    Added file downloading.

--- dspace-jspui/src/main/java/ua/edu/sumdu/essuir/service/ExportDocumentProcessorService.java
+++ dspace-jspui/src/main/java/ua/edu/sumdu/essuir/service/ExportDocumentProcessorService.java
@@ -6,11 +6,8 @@ import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTSectPr;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblWidth;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.STTblWidth;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import ua.edu.sumdu.essuir.entity.Publication;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Arrays;
@@ -31,9 +28,6 @@ public class ExportDocumentProcessorService {
         createTitle(author, document);
         createPublicationsTable(document, publications);
 
-        FileOutputStream out = new FileOutputStream(new File("C:/test/create_table.docx"));
-        document.write(out);
-        out.close();
         return document;
     }
 
@@ -70,7 +64,7 @@ public class ExportDocumentProcessorService {
 
     private void processRow(XWPFTable table, int rowIndex, String[] text) {
         XWPFTableRow row = table.getRow(rowIndex);
-        for(int cellIndex = 0; cellIndex < text.length; cellIndex++) {
+        for (int cellIndex = 0; cellIndex < text.length; cellIndex++) {
             XWPFTableCell cell = row.getCell(cellIndex);
             cell.setParagraph(tableCellParagraph(cell.getParagraph(), text[cellIndex], ParagraphAlignment.CENTER));
         }
@@ -80,16 +74,17 @@ public class ExportDocumentProcessorService {
         Integer[] width = {540, 2200, 1250, 3000, 1400, 2300};
         table.setWidth(Arrays.stream(width).reduce(0, (a, b) -> a + b));
         table.getCTTbl().addNewTblGrid().addNewGridCol().setW(BigInteger.valueOf(width[0]));
-        for (int col = 1 ; col < width.length; col++) {
+        for (int col = 1; col < width.length; col++) {
             table.getCTTbl().getTblGrid().addNewGridCol().setW(BigInteger.valueOf(width[col]));
         }
 
-        for(int i = 0; i < width.length; i++) {
+        for (int i = 0; i < width.length; i++) {
             CTTblWidth tblWidth = table.getRow(0).getCell(i).getCTTc().addNewTcPr().addNewTcW();
             tblWidth.setW(BigInteger.valueOf(width[i]));
             tblWidth.setType(STTblWidth.DXA);
         }
     }
+
     private void createPublicationsTable(XWPFDocument document, List<Publication> publications) {
         XWPFTable table = document.createTable(publications.size() + 1, 6);
         setTableProperties(table);
