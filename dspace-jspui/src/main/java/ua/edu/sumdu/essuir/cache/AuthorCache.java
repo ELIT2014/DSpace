4b8969107680aa2450fe9275d800f89ef0573465
Igor Marchenko <mar4enko.igor@gmail.com>
2018-07-20 12:44:43 +0300

    Updated orcid flow.

--- dspace-jspui/src/main/java/ua/edu/sumdu/essuir/cache/AuthorCache.java
+++ dspace-jspui/src/main/java/ua/edu/sumdu/essuir/cache/AuthorCache.java
@@ -44,7 +44,7 @@ public class AuthorCache {
 		return new ArrayList<String>(res);
 	}
 
-	public static String getOrcid(String authorName) {
+	public static Optional<String> getOrcid(String authorName) {
 		String res;
 
 		checkUpdate();
@@ -54,7 +54,7 @@ public class AuthorCache {
 			res = a != null ? a.getOrcid() : null;
 		}
 
-		return res;
+		return Optional.ofNullable(res);
 	}
 
     public static void makeLocalizedAuthors(List<DiscoverResult.FacetResult> authors, String locale, int facetPage) {
