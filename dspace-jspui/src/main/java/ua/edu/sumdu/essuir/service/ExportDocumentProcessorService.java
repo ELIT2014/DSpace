64aaca4987d0e04dfd045dfb144b7a59d3b3cadb
Igor Marchenko <mar4enko.igor@gmail.com>
2018-07-20 15:32:58 +0300

    Added bottom table.

--- dspace-jspui/src/main/java/ua/edu/sumdu/essuir/service/ExportDocumentProcessorService.java
+++ dspace-jspui/src/main/java/ua/edu/sumdu/essuir/service/ExportDocumentProcessorService.java
@@ -1,10 +1,7 @@
 package ua.edu.sumdu.essuir.service;
 
 import org.apache.poi.xwpf.usermodel.*;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTPageMar;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTSectPr;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTblWidth;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.STTblWidth;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.*;
 import org.springframework.stereotype.Component;
 import ua.edu.sumdu.essuir.entity.Publication;
 
@@ -27,7 +24,8 @@ public class ExportDocumentProcessorService {
 
         createTitle(author, document);
         createPublicationsTable(document, publications);
-
+        document.createParagraph().setSpacingAfter(100);
+        createBottomTable(document);
         return document;
     }
 
@@ -50,28 +48,27 @@ public class ExportDocumentProcessorService {
         document.createParagraph();
     }
 
-    private XWPFParagraph tableCellParagraph(XWPFParagraph paragraph, String text, ParagraphAlignment alignment) {
+    private XWPFParagraph tableCellParagraph(XWPFParagraph paragraph, String text, ParagraphAlignment alignment, int fontSize) {
         paragraph.setAlignment(alignment);
 
         XWPFRun run = paragraph.createRun();
         paragraph.setSpacingAfter(0);
-        run.setFontSize(14);
+        run.setFontSize(fontSize);
         run.setFontFamily("Times New Roman");
         run.getCTR().getRPr().getRFonts().setHAnsi("Times New Roman");
         run.setText(text);
         return paragraph;
     }
 
-    private void processRow(XWPFTable table, int rowIndex, String[] text) {
+    private void processRow(XWPFTable table, int rowIndex, String[] text, ParagraphAlignment alignment, int fontSize) {
         XWPFTableRow row = table.getRow(rowIndex);
         for (int cellIndex = 0; cellIndex < text.length; cellIndex++) {
             XWPFTableCell cell = row.getCell(cellIndex);
-            cell.setParagraph(tableCellParagraph(cell.getParagraph(), text[cellIndex], ParagraphAlignment.CENTER));
+            cell.setParagraph(tableCellParagraph(cell.getParagraph(), text[cellIndex], alignment, fontSize));
         }
     }
 
-    private void setTableProperties(XWPFTable table) {
-        Integer[] width = {540, 2200, 1250, 3000, 1400, 2300};
+    private void setTableProperties(XWPFTable table, Integer[] width) {
         table.setWidth(Arrays.stream(width).reduce(0, (a, b) -> a + b));
         table.getCTTbl().addNewTblGrid().addNewGridCol().setW(BigInteger.valueOf(width[0]));
         for (int col = 1; col < width.length; col++) {
@@ -87,9 +84,10 @@ public class ExportDocumentProcessorService {
 
     private void createPublicationsTable(XWPFDocument document, List<Publication> publications) {
         XWPFTable table = document.createTable(publications.size() + 1, 6);
-        setTableProperties(table);
+        Integer[] width = {540, 2200, 1250, 3000, 1400, 2300};
+        setTableProperties(table, width);
 
-        processRow(table, 0, new String[]{"№ з/п", "Назва", "Характер роботи", "Вихідні дані", "Обсяг", "Співавтори"});
+        processRow(table, 0, new String[]{"№ з/п", "Назва", "Характер роботи", "Вихідні дані", "Обсяг (у сторінках)/авторський доробок", "Співавтори"}, ParagraphAlignment.CENTER,  14);
         for (int index = 0; index < publications.size(); index++) {
             String[] rowData = {Integer.toString(index + 1),
                     publications.get(index).getTitle(),
@@ -97,8 +95,43 @@ public class ExportDocumentProcessorService {
                     publications.get(index).getCitation(),
                     "",
                     publications.get(index).getAuthors().replaceAll(";", ";\r\n").replaceAll(",", "")};
-            processRow(table, index + 1, rowData);
+            processRow(table, index + 1, rowData, ParagraphAlignment.CENTER, 14);
         }
+    }
+
+    private void createBottomTable(XWPFDocument document) {
+        XWPFTable table = document.createTable(5, 3);
+        Integer[] width = {3000, 2500, 3500};
+        setTableProperties(table, width);
+
+        CTTblPr tblpro = table.getCTTbl().getTblPr();
+
+        CTTblBorders borders = tblpro.addNewTblBorders();
+        borders.addNewBottom().setVal(STBorder.NONE);
+        borders.addNewLeft().setVal(STBorder.NONE);
+        borders.addNewRight().setVal(STBorder.NONE);
+        borders.addNewTop().setVal(STBorder.NONE);
+
+        borders.addNewInsideH().setVal(STBorder.NONE);
+        borders.addNewInsideV().setVal(STBorder.NONE);
+
+
+        table.getRow(0).setHeight(30);
+        processRow(table,  0, new String[]{"Автор або здобувач вченого звання (наукового ступеня)", "\r\n\r\n     ________________\r\n                 (підпис)", "\r\n\r\n     __________________________\r\n                           (прізвище, ініціали)"}, ParagraphAlignment.LEFT, 12);
+
+        table.getRow(1).setHeight(30);
+        processRow(table,  1, new String[]{"________________________\r\n                           (число, місяць, рік)", "", ""},ParagraphAlignment.LEFT, 12);
+
+        table.getRow(2).setHeight(30);
+        processRow(table,  2, new String[]{"Засвідчено:", "", ""}, ParagraphAlignment.LEFT, 12);
+
+        table.getRow(3).setHeight(30);
+        processRow(table,  3, new String[]{"Завідуючий (начальник) кафедрою", "\r\n\r\n     ________________\r\n                 (підпис)", "\r\n\r\n     __________________________\r\n                           (прізвище, ініціали)"}, ParagraphAlignment.LEFT, 12);
+
+        table.getRow(4).setHeight(30);
+        processRow(table,  4, new String[]{"Вчений секретар", "\r\n\r\n     ________________\r\n                 (підпис)", "\r\n\r\n     __________________________\r\n                           (прізвище, ініціали)"}, ParagraphAlignment.LEFT, 12);
+
+
     }
 
 }
