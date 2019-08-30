93e5c3afd45f55bf7b109efa06ce0baead9b58ee
Igor Marchenko <mar4enko.igor@gmail.com>
2019-08-30 11:55:24 +0300

    Display top communities and item count.

--- dspace-jspui/src/main/java/org/ssu/EssuirSiteController.java
+++ dspace-jspui/src/main/java/org/ssu/EssuirSiteController.java
@@ -1,5 +1,12 @@
 package org.ssu;
 
+import org.dspace.app.webui.util.UIUtil;
+import org.dspace.browse.ItemCountException;
+import org.dspace.browse.ItemCounter;
+import org.dspace.content.Community;
+import org.dspace.content.factory.ContentServiceFactory;
+import org.dspace.content.service.CommunityService;
+import org.dspace.core.Context;
 import org.dspace.core.I18nUtil;
 import org.dspace.core.factory.CoreServiceFactory;
 import org.dspace.core.service.NewsService;
@@ -11,10 +18,12 @@ import org.ssu.types.TypeLocalization;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequestMapping("/")
 @Controller
@@ -24,16 +33,33 @@ public class EssuirSiteController {
     private TypeLocalization typeLocalization;
 
     @RequestMapping("/")
-    public ModelAndView homePage(ModelAndView model, HttpServletRequest request) {
-        Locale locale = new Locale(Optional.ofNullable(request.getParameter("locale")).orElse("en"));
+    public ModelAndView homePage(ModelAndView model, HttpServletRequest request) throws SQLException, ItemCountException {
+        Context dspaceContext = UIUtil.obtainContext(request);
+        ItemCounter ic = new ItemCounter(dspaceContext);
+        Locale locale = dspaceContext.getCurrentLocale();
+
         NewsService newsService = CoreServiceFactory.getInstance().getNewsService();
         String topNews = newsService.readNewsFile(I18nUtil.getMessage("news-top.html", locale));
         String sideNews = newsService.readNewsFile(I18nUtil.getMessage("news-side.html", locale));
+        List<Community> communities = ContentServiceFactory.getInstance().getCommunityService().findAll(dspaceContext);
+
+        Map<Community, Integer> communityResponse = communities
+                .stream()
+                .filter(item -> item.getParentCommunities().isEmpty())
+                .collect(Collectors.toMap(item -> item, community -> {
+                    try {
+                        return ic.getCount(community);
+                    } catch (ItemCountException e) {
+
+                    }
+                    return 0;
+                }));
 
         List<ItemTypeResponse> submissionStatisticsByType = typeLocalization.getSubmissionStatisticsByType(locale.getLanguage());
         model.addObject("topNews", topNews);
         model.addObject("sideNews", sideNews);
         model.addObject("submissions", submissionStatisticsByType);
+        model.addObject("communities", communityResponse);
 
         model.setViewName("home");
         return model;
