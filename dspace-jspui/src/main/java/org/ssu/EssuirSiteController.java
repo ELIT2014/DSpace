1cfe411baafeb80ac33beec78b3486b3c63465cb
Igor Marchenko <mar4enko.igor@gmail.com>
2019-09-02 17:25:33 +0300

    Added ukraiinian localization.

--- dspace-jspui/src/main/java/org/ssu/EssuirSiteController.java
+++ dspace-jspui/src/main/java/org/ssu/EssuirSiteController.java
@@ -112,7 +112,7 @@ public class EssuirSiteController {
         Context dspaceContext = UIUtil.obtainContext(request);
         Locale locale = dspaceContext.getCurrentLocale();
         NewsService newsService = CoreServiceFactory.getInstance().getNewsService();
-        String faqFilePath = String.format("faqFilePath%s.html", locale.getLanguage().equals("en") ? "" : locale.getLanguage());
+        String faqFilePath = String.format("faq%s.html", locale.getLanguage().equals("en") ? "" : locale.getLanguage());
         model.addObject("faq", newsService.readNewsFile(faqFilePath));
         model.setViewName("faq");
         return model;
