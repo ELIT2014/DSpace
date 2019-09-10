d0940256c4d06fad9ac39bb4d2a8c9acda8522c9
Igor Marchenko <mar4enko.igor@gmail.com>
2019-09-10 16:43:50 +0300

    Saving entity to database.

--- dspace-jspui/src/main/java/org/ssu/entity/jooq/GeneralStatistics.java
+++ dspace-jspui/src/main/java/org/ssu/entity/jooq/GeneralStatistics.java
@@ -2,17 +2,22 @@ package org.ssu.entity.jooq;
 
 import org.jooq.Record;
 import org.jooq.TableField;
+import org.jooq.UniqueKey;
+import org.jooq.impl.Internal;
 import org.jooq.impl.SQLDataType;
 import org.jooq.impl.TableImpl;
+
+import java.util.Collections;
+import java.util.List;
 
 public class GeneralStatistics extends TableImpl<Record> {
     public static final GeneralStatistics TABLE = new GeneralStatistics();
 
-    public final TableField<Record, Integer> year = createField("year", SQLDataType.INTEGER);
-    public final TableField<Record, Integer> month = createField("month", SQLDataType.INTEGER);
+    public final TableField<Record, Integer> year = createField("year", SQLDataType.INTEGER.identity(true));
+    public final TableField<Record, Integer> month = createField("month", SQLDataType.INTEGER.identity(true));
     public final TableField<Record, Integer> viewCount = createField("count_views", SQLDataType.INTEGER);
     public final TableField<Record, Integer> downloadsCount = createField("count_downloads", SQLDataType.INTEGER);
-
+    public final UniqueKey<Record> primaryKey = Internal.createUniqueKey(TABLE, year, month);
 
     public GeneralStatistics() {
         super("general_statistics");
@@ -26,4 +31,15 @@ public class GeneralStatistics extends TableImpl<Record> {
     public GeneralStatistics as(String alias) {
         return new GeneralStatistics(this, alias);
     }
+
+    @Override
+    public UniqueKey<Record> getPrimaryKey() {
+        return Internal.createUniqueKey(TABLE, year, month);
+    }
+
+    @Override
+    public List<UniqueKey<Record>> getKeys() {
+        return Collections.singletonList(Internal.createUniqueKey(TABLE, year, month));
+    }
+
 }
--- dspace-jspui/src/main/java/org/ssu/repository/GeneralStatisticsRepository.java
+++ dspace-jspui/src/main/java/org/ssu/repository/GeneralStatisticsRepository.java
@@ -10,15 +10,10 @@ import java.util.stream.Collectors;
 
 @Service
 public class GeneralStatisticsRepository {
-    private static final org.ssu.entity.jooq.Item ITEM = org.ssu.entity.jooq.Item.TABLE;
-    private static final org.ssu.entity.jooq.Statistics STATISTICS = org.ssu.entity.jooq.Statistics.TABLE;
     private static final org.ssu.entity.jooq.GeneralStatistics GENERAL_STATISTICS = org.ssu.entity.jooq.GeneralStatistics.TABLE;
-    private static final org.ssu.entity.jooq.Metadatavalue METADATAVALUE = org.ssu.entity.jooq.Metadatavalue.TABLE;
-    private static final org.ssu.entity.jooq.Handle HANDLE = org.ssu.entity.jooq.Handle.TABLE;
 
     @Resource
     private DSLContext dsl;
-
 
     public List<GeneralStatistics> findAll() {
         return dsl.select(GENERAL_STATISTICS.asterisk())
@@ -28,53 +23,19 @@ public class GeneralStatisticsRepository {
                 .map(item -> new GeneralStatistics.Builder()
                         .withYear(item.get(GENERAL_STATISTICS.year))
                         .withMonth(item.get(GENERAL_STATISTICS.month))
-                        .withDownloadsCount(item.get(GENERAL_STATISTICS.downloadsCount).longValue())
-                        .withViewsCount(item.get(GENERAL_STATISTICS.viewCount).longValue())
+                        .withDownloadsCount(item.get(GENERAL_STATISTICS.downloadsCount))
+                        .withViewsCount(item.get(GENERAL_STATISTICS.viewCount))
                         .build()
                 ).collect(Collectors.toList());
     }
 
-    public List<GeneralStatistics> findAllYearsStatistics() {
-        return dsl.select(GENERAL_STATISTICS.asterisk())
-                .from(GENERAL_STATISTICS)
-                .where(GENERAL_STATISTICS.month.eq(-1))
-                .orderBy(GENERAL_STATISTICS.year.desc())
-                .fetch()
-                .stream()
-                .map(item -> new GeneralStatistics.Builder()
-                        .withYear(item.get(GENERAL_STATISTICS.year))
-                        .withMonth(item.get(GENERAL_STATISTICS.month))
-                        .withDownloadsCount(item.get(GENERAL_STATISTICS.downloadsCount).longValue())
-                        .withViewsCount(item.get(GENERAL_STATISTICS.viewCount).longValue())
-                        .build()
-                ).collect(Collectors.toList());
-
+    public void save(GeneralStatistics generalStatistics) {
+        dsl.insertInto(GENERAL_STATISTICS)
+                .set(GENERAL_STATISTICS.year, generalStatistics.getYear())
+                .set(GENERAL_STATISTICS.month, generalStatistics.getMonth())
+                .set(GENERAL_STATISTICS.viewCount, generalStatistics.getViewsCount())
+                .set(GENERAL_STATISTICS.downloadsCount, generalStatistics.getDownloadsCount())
+                .onDuplicateKeyIgnore()
+                .execute();
     }
-
-    public List<Integer> findAllMonthsViewsStatisticsByYear(Integer year) {
-        return dsl.select(GENERAL_STATISTICS.viewCount)
-                .from(GENERAL_STATISTICS)
-                .where(GENERAL_STATISTICS.year.eq(year).and(GENERAL_STATISTICS.month.notEqual(-1)))
-                .orderBy(GENERAL_STATISTICS.year)
-                .fetch(GENERAL_STATISTICS.viewCount);
-    }
-
-    public List<Integer> findAllMonthsDownloadsStatisticsByYear(Integer year) {
-        return dsl.select(GENERAL_STATISTICS.viewCount)
-                .from(GENERAL_STATISTICS)
-                .where(GENERAL_STATISTICS.year.eq(year).and(GENERAL_STATISTICS.month.notEqual(-1)))
-                .orderBy(GENERAL_STATISTICS.month)
-                .fetch(GENERAL_STATISTICS.viewCount);
-    }
-//    @Query("SELECT s FROM GeneralStatistics s WHERE s.month = -1 ORDER BY s.year DESC")
-//    List<GeneralStatistics> findAllYearsStatistics();
-
-//    @Query("SELECT s.viewsCount FROM GeneralStatistics s WHERE s.year = :year AND s.month <> -1 ORDER BY s.month")
-//    List<Integer> findAllMonthsViewsStatisticsByYear(@Param("year") Integer year);
-
-//    @Query("SELECT s.downloadsCount FROM GeneralStatistics s WHERE s.year = :year AND s.month <> -1 ORDER BY s.month")
-//    List<Integer> findAllMonthsDownloadsStatisticsByYear(@Param("year") Integer year);
-
-//    @Query("SELECT s FROM GeneralStatistics s WHERE s.year = :year AND s.month = :month")
-//    GeneralStatistics findCurrentYearTotalStatistics(@Param("year") Integer year, @Param("month") Integer month);
 }
