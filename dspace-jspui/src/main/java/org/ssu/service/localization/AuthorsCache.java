fcd1a1ade851af4caca0e7b575c7b8b92cebb77c
Igor Marchenko <mar4enko.igor@gmail.com>
2020-04-22 09:50:45 +0300

    Extract methods into repository class.

--- dspace-jspui/src/main/java/org/ssu/service/localization/AuthorsCache.java
+++ dspace-jspui/src/main/java/org/ssu/service/localization/AuthorsCache.java
@@ -3,6 +3,7 @@ package org.ssu.service.localization;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Service;
 import org.ssu.entity.AuthorLocalization;
+import org.ssu.repository.AuthorLocalizationRepository;
 import org.ssu.repository.DspaceObjectRepository;
 import org.ssu.service.EpersonService;
 
@@ -14,16 +15,8 @@ import java.util.stream.Stream;
 
 @Service
 public class AuthorsCache {
-    private static final org.ssu.entity.jooq.Authors AUTHORS = org.ssu.entity.jooq.Authors.TABLE;
-
     @Resource
-    private DSLContext dsl;
-
-    @Resource
-    private EpersonService epersonService;
-
-    @Resource
-    private DspaceObjectRepository dspaceObjectRepository;
+    private AuthorLocalizationRepository authorLocalizationRepository;
 
     private List<AuthorLocalization> authorLocalizations = new ArrayList<>();
     private Map<String, AuthorLocalization> englishMapping;
@@ -32,19 +25,7 @@ public class AuthorsCache {
 
     @PostConstruct
     public void updateCache() {
-        authorLocalizations = dsl.selectFrom(AUTHORS)
-                .fetch()
-                .stream()
-                .map(author ->
-                        new AuthorLocalization()
-                                .addAuthorData(author.get(AUTHORS.surnameEnglish), author.get(AUTHORS.initialsEnglish), Locale.ENGLISH)
-                                .addAuthorData(author.get(AUTHORS.surnameRussian), author.get(AUTHORS.initialsRussian), Locale.forLanguageTag("ru"))
-                                .addAuthorData(author.get(AUTHORS.surnameUkrainian), author.get(AUTHORS.initialsUkrainian), Locale.forLanguageTag("uk"))
-                                .setOrcid(author.get(AUTHORS.orcid))
-                                .setUuid(author.get(AUTHORS.uuid))
-
-                )
-                .collect(Collectors.toList());
+        authorLocalizations = authorLocalizationRepository.findAll();
 
         englishMapping = authorLocalizations.stream()
                 .collect(Collectors.toMap(author -> author.getFormattedAuthorData("%s, %s", Locale.ENGLISH), author -> author, (a, b) -> a));
@@ -83,54 +64,17 @@ public class AuthorsCache {
     }
 
     public void updateAuthorOrcid(AuthorLocalization author) {
-        dsl.update(AUTHORS)
-                .set(AUTHORS.orcid, author.getOrcid())
-                .where(AUTHORS.uuid.eq(author.getUuid()))
-                .execute();
+        authorLocalizationRepository.updateAuthorOrcid(author);
         updateCache();
     }
 
     public void removeAuthorData(UUID uuid) {
-        dsl.delete(AUTHORS)
-                .where(AUTHORS.uuid.eq(uuid))
-                .execute();
+        authorLocalizationRepository.deleteByUuid(uuid);
         updateCache();
     }
 
-
-    private UUID resolveEpersonUuidIfUserExists(AuthorLocalization author) {
-        return Stream.of(
-                epersonService.findEpersonUuuidByPersonalName(author.getInitials(Locale.ENGLISH), author.getSurname(Locale.ENGLISH)),
-                epersonService.findEpersonUuuidByPersonalName(author.getInitials(Locale.forLanguageTag("ru")), author.getSurname(Locale.forLanguageTag("ru"))),
-                epersonService.findEpersonUuuidByPersonalName(author.getInitials(Locale.forLanguageTag("uk")), author.getSurname(Locale.forLanguageTag("uk"))))
-                .filter(Optional::isPresent)
-                .reduce((a, b) -> a)
-                .map(Optional::get)
-                .orElse(author.getUuid());
-    }
-
     public void updateAuthorData(AuthorLocalization author) {
-
-        dspaceObjectRepository.insertUuid(resolveEpersonUuidIfUserExists(author));
-
-        dsl.insertInto(AUTHORS)
-                .set(AUTHORS.surnameEnglish, author.getSurname(Locale.ENGLISH))
-                .set(AUTHORS.initialsEnglish, author.getInitials(Locale.ENGLISH))
-                .set(AUTHORS.surnameRussian, author.getSurname(Locale.forLanguageTag("ru")))
-                .set(AUTHORS.initialsRussian, author.getInitials(Locale.forLanguageTag("ru")))
-                .set(AUTHORS.surnameUkrainian, author.getSurname(Locale.forLanguageTag("uk")))
-                .set(AUTHORS.initialsUkrainian, author.getInitials(Locale.forLanguageTag("uk")))
-                .set(AUTHORS.orcid, author.getOrcid())
-                .set(AUTHORS.uuid, author.getUuid())
-                .onDuplicateKeyUpdate()
-                .set(AUTHORS.surnameEnglish, author.getSurname(Locale.ENGLISH))
-                .set(AUTHORS.initialsEnglish, author.getInitials(Locale.ENGLISH))
-                .set(AUTHORS.surnameRussian, author.getSurname(Locale.forLanguageTag("ru")))
-                .set(AUTHORS.initialsRussian, author.getInitials(Locale.forLanguageTag("ru")))
-                .set(AUTHORS.surnameUkrainian, author.getSurname(Locale.forLanguageTag("uk")))
-                .set(AUTHORS.initialsUkrainian, author.getInitials(Locale.forLanguageTag("uk")))
-                .set(AUTHORS.orcid, author.getOrcid())
-                .execute();
+        authorLocalizationRepository.updateAuthorData(author);
         updateCache();
     }
 
